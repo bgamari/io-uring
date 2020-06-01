c4250772ec11aeb317b7785073d1580ea349fd59
Ben Gamari <ben@smart-cactus.org>
2020-06-01 11:02:35 -0400

    Clean up C wrapper

--- cbits/hs_uring.c
+++ cbits/hs_uring.c
@@ -24,14 +24,14 @@ static size_t sq_len(const struct io_uring_params *params)
   return params->sq_off.array + params->sq_entries * sizeof(__u32);
 }
 
-static size_t cq_len(const struct io_uring_params *params)
-{
-  return params->cq_off.cqes + params->cq_entries * sizeof(struct io_uring_cqe);
-}
-
 static size_t sqe_len(const struct io_uring_params *params)
 {
   return params->sq_entries * sizeof(struct io_uring_sqe);
+}
+
+static size_t cq_len(const struct io_uring_params *params)
+{
+  return params->cq_off.cqes + params->cq_entries * sizeof(struct io_uring_cqe);
 }
 
 struct hs_uring *hs_new_uring(unsigned int entries)
@@ -45,7 +45,7 @@ struct hs_uring *hs_new_uring(unsigned int entries)
 
   struct io_uring_params *params = &uring->params;
   const int fd = io_uring_setup(entries, params);
-  if (fd == -1) {
+  if (fd < 0) {
     fprintf(stderr, "hs_new_uring: failed create uring: %s\n", strerror(errno));
     goto error;
   }
@@ -53,21 +53,21 @@ struct hs_uring *hs_new_uring(unsigned int entries)
 
   // SQ Ring
   uring->sq_aperture = mmap_aperture(fd, sq_len(params), IORING_OFF_SQ_RING);
-  if (uring->sq_aperture == NULL) {
+  if (uring->sq_aperture == MAP_FAILED) {
     fprintf(stderr, "hs_new_uring: failed to map SQ aperture: %s\n", strerror(errno));
     goto error;
   }
 
   // SQE aperature
   uring->sqe_aperture = mmap_aperture(fd, sqe_len(params), IORING_OFF_SQES);
-  if (uring->sq_aperture == NULL) {
+  if (uring->sqe_aperture == MAP_FAILED) {
     fprintf(stderr, "hs_new_uring: failed to map SQE aperture: %s\n", strerror(errno));
     goto error;
   }
 
   // CQ Ring
   uring->cq_aperture = mmap_aperture(fd, cq_len(params), IORING_OFF_CQ_RING);
-  if (uring->cq_aperture == NULL) {
+  if (uring->cq_aperture == MAP_FAILED) {
     fprintf(stderr, "hs_new_uring: failed to map CQ aperture: %s\n", strerror(errno));
     goto error;
   }
@@ -82,16 +82,16 @@ error:
 void hs_free_uring(struct hs_uring *uring) {
   const struct io_uring_params *params = &uring->params;
 
-  if (uring->cq_aperture) {
+  if (uring->cq_aperture != MAP_FAILED) {
     munmap(uring->cq_aperture, cq_len(params));
   }
 
-  if (uring->sq_aperture) {
-    munmap(uring->sq_aperture, sq_len(params));
+  if (uring->sqe_aperture != MAP_FAILED) {
+    munmap(uring->sqe_aperture, sqe_len(params));
   }
 
-  if (uring->sqe_aperture) {
-    munmap(uring->sqe_aperture, sqe_len(params));
+  if (uring->sq_aperture != MAP_FAILED) {
+    munmap(uring->sq_aperture, sq_len(params));
   }
 
   free(uring);
